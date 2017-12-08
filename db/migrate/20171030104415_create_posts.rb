d3d985fcdd8f2725886a5b94c33f7e61c4648ddd
Unknown <paltaxa12345@gmail.com>
2017-12-08 17:23:39 +0200

    Update

--- db/migrate/20171030104415_create_posts.rb
+++ db/migrate/20171030104415_create_posts.rb
@@ -2,8 +2,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :summary
-      t.text :body
+      t.string :body
+      t.integer :user_id
 
       t.timestamps
     end
